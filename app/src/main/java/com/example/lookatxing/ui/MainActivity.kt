8be4943711167ae95a2da390bce2ab430ee68854
Felipe Zadinello <felipezad@gmail.com>
2021-03-26 15:58:16 -0300

    Add hilt entry point for main activity

--- app/src/main/java/com/example/lookatxing/ui/MainActivity.kt
+++ app/src/main/java/com/example/lookatxing/ui/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.lookatxing.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lookatxing.R
+import dagger.hilt.android.AndroidEntryPoint
 
-//@AndroidEntryPoint
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
